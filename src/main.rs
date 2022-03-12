8e94ca6f495348e87473787bd298dea1abf9c677
bercivarga <65171545+bercivarga@users.noreply.github.com>
2022-03-12 14:32:41 +0100

    Add core loop logic

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,18 @@
 use std::error::Error;
+use std::io;
 
-fn main() {}
+fn main() -> Result<(), Box<dyn Error>> {
+    println!("Wordle in your terminal!");
+    loop {
+        println!("Enter your guess!");
 
-async fn get_daily_word() -> Result<(), Box<dyn Error>> {
-    let resp = reqwest::blocking::get("https://httpbin.org/ip")?.text()?;
+        let mut guess = String::new();
+
+        io::stdin().read_line(&mut guess).expect("Faulty input.");
+        let resp =
+            reqwest::blocking::get(format!("https://v1.wordle.k2bd.dev/daily?guess={}", guess))?
+                .text()?;
+        println!("{:#?}", resp);
+    }
     Ok(())
 }
