afacf58ffae0f9b5d27f3d038859177c2b65fb88
bercivarga <65171545+bercivarga@users.noreply.github.com>
2022-03-12 16:26:40 +0100

    add coloring logic

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use colored::*;
 use serde::Deserialize;
 use serde_json;
 use std::error::Error;
@@ -5,19 +6,38 @@ use std::io;
 
 #[derive(Deserialize, Debug)]
 struct GuessSlot {
-    slot: u8,
     guess: String,
     result: String,
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
-    println!("Wordle in your terminal!");
+    println!("🔠 Wordle in your terminal!");
+
+    let tries_left = 6;
+    let did_win = false;
+    let mut solutions: Vec<[GuessSlot; 5]> = vec![];
+
+    println!("Start guessing:");
+
     loop {
-        println!("Enter your guess!");
+        if tries_left == 0 && !did_win {
+            println!("You lost");
+            break;
+        } else if did_win {
+            print_rows(&solutions);
+            println!("You won! 🎉");
+        }
 
         let mut guess = String::new();
 
         io::stdin().read_line(&mut guess).expect("Faulty input.");
+
+        if guess.len() - 1 < 5 || guess.len() - 1 > 5 {
+            println!("You need to insert 5 characters!");
+            println!("You types {} characters.", guess.len() - 1);
+            println!("Game over...");
+            break;
+        }
 
         let resp =
             reqwest::blocking::get(format!("https://v1.wordle.k2bd.dev/daily?guess={}", guess))?
@@ -29,10 +49,29 @@ fn main() -> Result<(), Box<dyn Error>> {
             .filter(|x| x.to_owned() != "".to_owned())
             .collect::<Vec<&str>>();
         filtered_split_guess.pop();
-        println!("{:?}", filtered_split_guess);
+
         let result: [GuessSlot; 5] = serde_json::from_str(&resp).unwrap();
-        println!("{:?}", result);
-        break;
+        solutions.push(result);
+
+        print_rows(&solutions);
     }
     Ok(())
+}
+
+fn print_rows(solutions: &Vec<[GuessSlot; 5]>) {
+    for solution in solutions {
+        for slot in solution {
+            print!(
+                "[{}]",
+                match slot.result.as_str() {
+                    "correct" => slot.guess.to_string().to_uppercase().green(),
+                    "present" => slot.guess.to_string().to_uppercase().yellow(),
+                    "absent" => slot.guess.to_string().to_uppercase().red(),
+                    _ => slot.guess.to_string().to_uppercase().white(),
+                }
+            );
+        }
+        print!("\n");
+    }
+    println!("----------------");
 }
